267af8ba48a5dbe58f40da0d71cff8aaefed5a07
ArmorDRX97 <armor.d.r.x@gmail.com>
2020-09-14 03:36:47 +0600

    test

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,13 +2,11 @@
     <div class="app-main-layout">
         <Navbar  @click="isOpen = !isOpen"/>
         <Sidebar v-model="isOpen"/>
-
         <main class="app-content" :class="{full: !isOpen}">
             <div class="app-page">
                 <router-view/>
             </div>
         </main>
-
         <div class="fixed-action-btn">
             <router-link class="btn-floating btn-large blue" to="/record">
                 <i class="large material-icons">add</i>
