f5cedbe8b362a119b8345d164bc1e4d207e1e782
ArmorDRX97 <armor.d.r.x@gmail.com>
2020-09-14 03:49:41 +0600

    first branch

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,11 +2,19 @@
     <div class="app-main-layout">
         <Navbar  @click="isOpen = !isOpen"/>
         <Sidebar v-model="isOpen"/>
+<<<<<<< HEAD
+=======
+
+>>>>>>> first commit
         <main class="app-content" :class="{full: !isOpen}">
             <div class="app-page">
                 <router-view/>
             </div>
         </main>
+<<<<<<< HEAD
+=======
+
+>>>>>>> first commit
         <div class="fixed-action-btn">
             <router-link class="btn-floating btn-large blue" to="/record">
                 <i class="large material-icons">add</i>
